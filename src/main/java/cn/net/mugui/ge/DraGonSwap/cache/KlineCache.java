deab5ce3f76475a6d39acf90724e12345de773da
zx844174097 <zx844174097@126.com>
2021-03-18 02:00:18 +0800

    123

--- src/main/java/cn/net/mugui/ge/DraGonSwap/cache/KlineCache.java
+++ src/main/java/cn/net/mugui/ge/DraGonSwap/cache/KlineCache.java
@@ -2,7 +2,6 @@ package cn.net.mugui.ge.DraGonSwap.cache;
 
 import javax.servlet.http.HttpSession;
 
-import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Component;
 
 import com.mugui.spring.base.Cache;
@@ -21,21 +20,20 @@ public class KlineCache implements CacheModel {
 	private static long GlobalCache_time = 5000;
 	private static TimedCache<String, Message> map = new TimedCache<String, Message>(GlobalCache_time);
 
+	private String getKey(NetBag bag) {
+		return (bag.getFunc() + Other.MD5(bag.getData().toString()));
+	}
+
 	@Override
 	public NetBag load(NetBag bag) {
-		String str = String.valueOf(bag.getData());
-		if (str.length() > 512) {
-			str = str.substring(0, 512);
-		}
-		str = str.replaceAll("\\s*", "");
-		String string = (bag.getFunc() + Other.MD5(str));
+		String string = getKey(bag);
 		Message bean = null;
 
 		if ((bean = map.get(string, false)) != null) {
 			bag.setData(bean);
 			return null;
 		}
-		SessionImpl.getSession(bag).setAttribute("method:" + bag.getFunc(), string);
+//		SessionImpl.getSession(bag).setAttribute("method:" + bag.getFunc(), string);
 		return bag;
 	}
 
@@ -44,11 +42,7 @@ public class KlineCache implements CacheModel {
 		if (message.getType() == Message.SUCCESS) {
 			HttpSession session = SessionImpl.getSession(bag);
 			if (session != null) {
-				String string = (String) session.getAttribute("method:" + bag.getFunc());
-				if (StringUtils.isNotBlank(string)) {
-					map.put(string, message);
-					session.removeAttribute("method:" + bag.getFunc());
-				}
+				map.put(getKey(bag), message);
 			}
 		}
 	}
