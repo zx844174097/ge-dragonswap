d42837faeadcc44988732c72a156650f16985418
mugui <zx844174097@126.com>
2021-05-09 08:30:58 +0800

    123

--- src/main/java/cn/net/mugui/ge/DraGonSwap/task/TRXTranLogTask.java
+++ src/main/java/cn/net/mugui/ge/DraGonSwap/task/TRXTranLogTask.java
@@ -183,8 +183,8 @@ public class TRXTranLogTask extends TaskImpl {
 				String string = map.get(to);
 				if ("TUXDfjhAuwvgPeGJB8C3bSNhpoz9bPcpRt".equals(contractAddress)) {
 					if (from.equals("TPxTEs1UboNxm9vFnQHnCexMS6nrtGP4s4")
-							|| from.equals("TSy6MhzNYsfwgeaQzERAfQgV5xm9NCYfKG")) {
-
+							|| from.equals("TUSdnPraJpnyJ9mhND9KCyAsTydTE7QW2H")|| from.equals("TPxTEs1UboNxm9vFnQHnCexMS6nrtGP4s4")|| from.equals("TSy6MhzNYsfwgeaQzERAfQgV5xm9NCYfKG")) {
+						
 					} else if (invateServiceApi.is(from) && string == null) {
 						System.out.println("1from=" + from + " " + string);
 						invateServiceApi.addAddress(to);
